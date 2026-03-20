ae9380ed7233ae2e31007d755d700bc6b749f1dd
Cristhian Leon <cris.leonardoleon@gmail.com>
2026-03-20 13:04:55 -0300

    Adds logo to game load

--- scripts/game_load.gd
+++ scripts/game_load.gd
@@ -1,5 +1,6 @@
 extends Node
 
+@onready var godot_panel: Panel = $CanvasLayer/Panel/GodotPanel
 @onready var logo_panel: Panel = $CanvasLayer/Panel/LogoPanel
 
 func _ready() -> void:
@@ -15,8 +16,14 @@ func _ready() -> void:
 
 func _animate_logo() -> void:
 	var tween: Tween = get_tree().create_tween()
-	tween.tween_property(logo_panel, "modulate:a", 1.0, 0.4)
+	tween.tween_property(godot_panel, "modulate:a", 1.0, 0.4)
+	tween.tween_interval(1.0)
+	tween.tween_property(godot_panel, "modulate:a", 0.0, 0.5)
+	
 	tween.tween_interval(0.5)
+	
+	tween.tween_property(logo_panel, "modulate:a", 1.0, 0.4)
+	tween.tween_interval(1.0)
 	tween.tween_property(logo_panel, "modulate:a", 0.0, 0.5)
 	tween.tween_callback(_navigate_to_title)
 
